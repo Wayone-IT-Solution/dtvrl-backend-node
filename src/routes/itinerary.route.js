d335de256f58d48fda9d0de510c2ff4b8aa68e8c
Deepak Singh <deepaky954000@gmail.com>
2025-05-22 16:11:06 +0530

    added authenticatoin in itinerary

--- src/routes/itinerary.route.js
+++ src/routes/itinerary.route.js
@@ -5,13 +5,15 @@ import { authentication } from "#middlewares/authentication";
 
 const router = express.Router();
 
-// router.use(authentication);
+router.use(authentication);
 
 router
   .route("/:id?")
   .get(asyncHandler(ItineraryController.get.bind(ItineraryController)))
   .post(asyncHandler(ItineraryController.create.bind(ItineraryController)))
   .put(asyncHandler(ItineraryController.update.bind(ItineraryController)))
-  .delete(asyncHandler(ItineraryController.deleteDoc.bind(ItineraryController)));
+  .delete(
+    asyncHandler(ItineraryController.deleteDoc.bind(ItineraryController)),
+  );
 
 export default router;
