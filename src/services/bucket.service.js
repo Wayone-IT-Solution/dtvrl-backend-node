35f5de72bf26fc45a06527b3abee5773fe70f44c
Deepak Singh <deepaky954000@gmail.com>
2025-06-04 15:32:13 +0530

    fixed few changes

--- src/services/bucket.service.js
+++ src/services/bucket.service.js
@@ -3,6 +3,11 @@ import BaseService from "#services/base";
 
 class BucketService extends BaseService {
   static Model = Bucket;
+
+  static async deleteDoc(id) {
+    const doc = await this.Model.findDocById(id);
+    await doc.destroy({ force: true });
+  }
 }
 
 export default BucketService;
