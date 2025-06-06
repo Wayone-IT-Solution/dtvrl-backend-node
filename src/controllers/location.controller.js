1c81235d9f4a0481a75514b4993de1c86b1490c9
Amandeep Kaur <amank.secure4@gmail.com>
2025-06-06 16:20:29 +0530

    Formatted data for location module

--- src/controllers/location.controller.js
+++ src/controllers/location.controller.js
@@ -1,8 +1,31 @@
 import LocationService from "#services/location";
 import BaseController from "#controllers/base";
+import LocationReview from "#models/locationReview";
+import { sendResponse } from "#utils/response";
+import httpStatus from "http-status";
 
 class LocationController extends BaseController {
   static Service = LocationService;
+
+  static async get(req, res, next) {
+    const { id } = req.params;
+    const customOptions = {
+      include: [
+        {
+          model: LocationReview,
+        },
+      ],
+    };
+
+    const options = this.Service.getOptions(req.query, customOptions);
+    const data = await this.Service.get(id, req.query, options);
+    sendResponse(
+      httpStatus.OK,
+      res,
+      data,
+      `${this.Service.Model.updatedName()} fetched successfully`,
+    );
+  }
 }
 
 export default LocationController;
