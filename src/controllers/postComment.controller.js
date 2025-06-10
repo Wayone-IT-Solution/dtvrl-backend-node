ef084105fd63be2fea9afb63fef49fba4df2243f
Deepak Singh <deepak@wayone.co.in>
2025-06-10 16:47:11 +0530

    fixed auth error in post comment

--- src/controllers/postComment.controller.js
+++ src/controllers/postComment.controller.js
@@ -1,8 +1,15 @@
 import BaseController from "#controllers/base";
+import { session } from "#middlewares/requestSession";
 import PostCommentService from "#services/postComment";
 
 class PostCommentController extends BaseController {
   static Service = PostCommentService;
+
+  static async create(req, res, next) {
+    const userId = session.get("userId");
+    req.body.userId = userId;
+    return await super.create(req, res, next);
+  }
 }
 
 export default PostCommentController;
