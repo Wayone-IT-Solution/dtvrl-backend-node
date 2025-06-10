b271c21e2ca1b00a680c64c704cc38edadcaa3b4
Deepak Singh <deepak@wayone.co.in>
2025-06-10 17:52:28 +0530

    Added user details in post comments

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -3,6 +3,7 @@ import AdminService from "#services/admin";
 import BaseController from "#controllers/base";
 import { sendResponse } from "#utils/response";
 import { session } from "#middlewares/requestSession";
+import UserService from "#services/user";
 
 class AdminController extends BaseController {
   static Service = AdminService;
@@ -16,6 +17,12 @@ class AdminController extends BaseController {
     const adminId = session.get("userId");
     const loggedInUser = await this.Service.getDocById(adminId);
     sendResponse(httpStatus.OK, res, loggedInUser);
+  }
+
+  static async getUsers(req, res, next) {
+    const options = this.Service.getOptions(req.query, {});
+    const users = await UserService.get(null, req.query, options);
+    sendResponse(httpStatus.OK, res, users, "Users fetched successfully");
   }
 }
 
--- src/controllers/postComment.controller.js
+++ src/controllers/postComment.controller.js
@@ -1,6 +1,8 @@
 import BaseController from "#controllers/base";
 import { session } from "#middlewares/requestSession";
+import { sendResponse } from "#utils/response";
 import PostCommentService from "#services/postComment";
+import httpStatus from "http-status";
 
 class PostCommentController extends BaseController {
   static Service = PostCommentService;
@@ -9,6 +11,23 @@ class PostCommentController extends BaseController {
     const userId = session.get("userId");
     req.body.userId = userId;
     return await super.create(req, res, next);
+  }
+
+  static async get(req, res, next) {
+    const customOptions = {
+      include: [
+        {
+          model: User,
+          attributes: ["id", "name", "username", "profile", "email"],
+        },
+      ],
+    };
+
+    const { id } = req.params;
+
+    const options = this.Service.getOptions(req.query, customOptions);
+    const data = await this.Service(id, req.query, options);
+    sendResponse(httpStatus.OK, res, data);
   }
 }
 
