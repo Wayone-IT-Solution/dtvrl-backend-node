c1f5b6693f29ed1235af074d99bea66d2714d68c
Deepak Singh <deepak@wayone.co.in>
2025-06-11 18:36:13 +0530

    fixed session error

--- src/controllers/postView.controller.js
+++ src/controllers/postView.controller.js
@@ -1,5 +1,6 @@
 import PostViewService from "#services/postView";
 import BaseController from "#controllers/base";
+import { session } from "#middlewares/requestSession";
 
 class PostViewController extends BaseController {
   static Service = PostViewService;
