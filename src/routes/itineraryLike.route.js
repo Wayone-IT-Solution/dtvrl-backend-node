cd3bab307ea16903ffbfe2908367d2938f0c98fe
Deepak Singh <deepak@wayone.co.in>
2025-06-11 16:45:49 +0530

    Fixed auth error

--- src/routes/itineraryLike.route.js
+++ src/routes/itineraryLike.route.js
@@ -5,13 +5,21 @@ import { authentication } from "#middlewares/authentication";
 
 const router = express.Router();
 
-// router.use(authentication);
+router.use(authentication);
 
 router
   .route("/:id?")
   .get(asyncHandler(ItineraryLikeController.get.bind(ItineraryLikeController)))
-  .post(asyncHandler(ItineraryLikeController.create.bind(ItineraryLikeController)))
-  .put(asyncHandler(ItineraryLikeController.update.bind(ItineraryLikeController)))
-  .delete(asyncHandler(ItineraryLikeController.deleteDoc.bind(ItineraryLikeController)));
+  .post(
+    asyncHandler(ItineraryLikeController.create.bind(ItineraryLikeController)),
+  )
+  .put(
+    asyncHandler(ItineraryLikeController.update.bind(ItineraryLikeController)),
+  )
+  .delete(
+    asyncHandler(
+      ItineraryLikeController.deleteDoc.bind(ItineraryLikeController),
+    ),
+  );
 
 export default router;
