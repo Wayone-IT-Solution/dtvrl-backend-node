782aabc09c20835af8c2aff9eeebc083449ff648
Deepak Singh <deepak@wayone.co.in>
2025-06-11 12:30:11 +0530

    updated location review comment API

--- src/controllers/locationReviewComment.controller.js
+++ src/controllers/locationReviewComment.controller.js
@@ -1,6 +1,8 @@
 import LocationReviewCommentService from "#services/locationReviewComment";
 import BaseController from "#controllers/base";
 import { session } from "#middlewares/requestSession";
+import { sendResponse } from "#utils/response";
+import httpStatus from "http-status";
 
 class LocationReviewCommentController extends BaseController {
   static Service = LocationReviewCommentService;
@@ -8,7 +10,36 @@ class LocationReviewCommentController extends BaseController {
   static async create(req, res, next) {
     const userId = session.get("userId");
     req.body.userId = userId;
-    return await super.create(req, res, next);
+    const postComment = await this.Service.create(req.body);
+    const updatedData = await this.Service.getDoc(
+      { id: postComment.id },
+      {
+        transaction: session.get("transaction"),
+        include: [
+          {
+            model: User,
+            attributes: ["id", "name", "profile", "username"],
+          },
+        ],
+      },
+    );
+    sendResponse(httpStatus.OK, res, updatedData);
+  }
+
+  static async get(req, res, next) {
+    const { id } = req.params;
+
+    const customOptions = {
+      include: [
+        {
+          model: User,
+          attributes: ["id", "name", "profile", "username"],
+        },
+      ],
+    };
+    const options = this.Service.getOptions(req.query, customOptions);
+    const data = await this.Service.get(id, req.query, options);
+    sendResponse(httpStatus.OK, res, data);
   }
 }
 
