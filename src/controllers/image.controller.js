3679e208b914dc773d5018fc47ad8117b2663bce
coolboyalan <deepaky954000@gmail.com>
2025-06-12 18:03:58 +0530

    Added image log

--- src/controllers/image.controller.js
+++ src/controllers/image.controller.js
@@ -3,6 +3,11 @@ import BaseController from "#controllers/base";
 
 class ImageController extends BaseController {
   static Service = ImageService;
+
+  static async create(req, res, next) {
+    console.log(req.files);
+    console.log(req.body);
+  }
 }
 
 export default ImageController;
