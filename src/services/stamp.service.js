49b7da283a8d3635a2af38660bb588d75409522e
Deepak Singh <deepaky954000@gmail.com>
2025-06-30 11:41:22 +0530

    Added stamp delete Service

--- src/services/stamp.service.js
+++ src/services/stamp.service.js
@@ -3,6 +3,12 @@ import BaseService from "#services/base";
 
 class StampService extends BaseService {
   static Model = Stamp;
+
+  static async deleteDoc(id) {
+    const doc = await StampService.getDocById(id);
+    await doc.destroy({ force: true });
+    return null;
+  }
 }
 
 export default StampService;
